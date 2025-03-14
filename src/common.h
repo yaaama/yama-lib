e2415d2279502d96307b7d7d4a76645d9a67eda9
Sau P <sau@yaaama.xyz>
2025-03-14 18:10:39 +0000

    common.h: New macros and other useful things

--- src/common.h
+++ src/common.h
@@ -19,11 +19,41 @@ typedef long long i64;
 typedef float f32;
 typedef double f64;
 
-
 #define true 1
 #define false 0
 
-#define null 0
 #define u32_max 4294967295
 
-#endif // COMMON_H_
+#define YAMA_ASSERT assert
+#define YAMA_UNUSED(value) (void) (value)
+#define YAMA_ARRAY_LENGTH(a) (sizeof((a)) / sizeof(((a)[0])))
+#define YAMA_ARRAY_GET(array, index)                        \
+    (YAMA_ASSERT((size_t) (index) < YAMA_ARRAY_LEN(array)), \
+     (array)[(size_t) (index)])
+
+#ifdef NDEBUG
+    #define YAMA_TODO(message) ((void) 0)
+    #define YAMA_UNREACHABLE(message) ((void) 0)
+#else
+    /* If stdio is not included then lets define it */
+    #if !defined(_STDIO_H_) && !defined(_STDIO_H)
+int printf(const char *restrict format, ...);
+int fprintf(int *restrict stream, const char *restrict format, ...);
+    #endif
+
+    /* Define our debugging macros */
+    #define YAMA_TODO(message)                                                 \
+        do {                                                                   \
+            fprintf(stderr, "%s:%d: TODO: %s\n", __FILE__, __LINE__, message); \
+            abort();                                                           \
+        } while (0)
+    #define YAMA_UNREACHABLE(message)                                       \
+        do {                                                                \
+            fprintf(stderr, "%s:%d: UNREACHABLE: %s\n", __FILE__, __LINE__, \
+                    message);                                               \
+            abort();                                                        \
+        } while (0)
+
+#endif
+
+#endif  // COMMON_H_
